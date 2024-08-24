bb31c4fa4911a7d088399eae20f86e763820414a
andreihar <95883512+andreihar@users.noreply.github.com>
2024-08-24 00:20:05 -0700

    Added logo

--- main.m
+++ main.m
@@ -1,25 +1,20 @@
 close all
 clear variables
 
-data_dir = './data';
-out_dir = './results';
-
 source = imread('./data/source_01.jpg');
 mask = imread('./data/mask_01.jpg');
 target = imread('./data/target_01.jpg');
 
 offset = [ 210 10 ];
-
 source = im2double(source);
 mask = round(im2double(mask));
 target = im2double(target);
 
-% Interactive mask
-mask = getmask(source);
+% mask = getmask(source);
 [source, mask, target] = fiximages(source, mask, target, offset);
 
 output = imblend(source, mask, target);
 
 figure();
 imshow(output);
-% imwrite(output,sprintf('%s/result_%02d.jpg',out_dir,i),'jpg','Quality',95);
+% imwrite(output,'%s/result.jpg','jpg','Quality',95);
